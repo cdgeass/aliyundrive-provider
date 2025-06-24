5c0a299075db7f599f8e11f7360da610dc0c7995
cdgeass <cdgeass97@gmail.com>
2025-06-24 17:41:59 +0800

    support thumbnail

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "io.github.cdgeass.aliyunpanprovider"
-        minSdk = 34
+        minSdk = 32
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -28,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
--- app/src/main/java/io/github/cdgeass/aliyunpanprovider/AliyunpanProvider.kt
+++ app/src/main/java/io/github/cdgeass/aliyunpanprovider/AliyunpanProvider.kt
@@ -2,18 +2,23 @@ package io.github.cdgeass.aliyunpanprovider
 
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import android.content.res.AssetFileDescriptor
 import android.database.Cursor
 import android.database.MatrixCursor
+import android.graphics.Point
 import android.os.CancellationSignal
 import android.os.ParcelFileDescriptor
 import android.provider.DocumentsContract
 import android.provider.DocumentsProvider
 import android.util.Log
+import androidx.core.content.edit
 import io.github.cdgeass.AliyunpanClient
+import io.github.cdgeass.model.FileItem
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okio.FileNotFoundException
 import java.io.File
+import java.time.Instant
 
 class AliyunpanProvider : DocumentsProvider() {
 
@@ -38,28 +43,47 @@ class AliyunpanProvider : DocumentsProvider() {
         )
     }
 
-    private val sharedPreferences: SharedPreferences? by lazy {
-        context?.getSharedPreferences("AliyunpanProvider", MODE_PRIVATE)
+    private val sharedPreferences: SharedPreferences by lazy {
+        context!!.getSharedPreferences("AliyunpanProvider", MODE_PRIVATE)
     }
 
     private val client: AliyunpanClient = AliyunpanClient()
 
-    private lateinit var authorization: String
-    private lateinit var backupDriveId: String
-    private lateinit var resourceDriveId: String
+    private val authorization: String?
+        get() {
+            return sharedPreferences.getString("authorization", null)
+        }
+    private var backupDriveId: String
+        set(value) {
+            sharedPreferences.edit { putString("backupDriveId", value) }
+        }
+        get() {
+            return sharedPreferences.getString("backupDriveId", null) ?: ""
+        }
+    private var resourceDriveId: String
+        set(value) {
+            sharedPreferences.edit { putString("resourceDriveId", value) }
+        }
+        get() {
+            return sharedPreferences.getString("resourceDriveId", null) ?: ""
+        }
 
     override fun onCreate(): Boolean {
-        authorization = sharedPreferences?.getString("authorization", null) ?: return false
+        val authorization = authorization ?: return false
 
         try {
             val getUserResponse = client.getUser(authorization).get()
             backupDriveId = getUserResponse.backupDriveId
             resourceDriveId = getUserResponse.resourceDriveId
         } catch (e: Exception) {
-            Log.e("AliyunpanProvider", "Failed to get authorization", e)
+            Log.e("AliyunpanProvider", "Failed to get authorization ${e.message}", e)
             return false
         }
 
+        Log.d(
+            "AliyunpanProvider",
+            "onCreate backupDriveId: $backupDriveId; resourceDriveId: $resourceDriveId"
+        )
         return true
     }
 
@@ -68,17 +92,23 @@ class AliyunpanProvider : DocumentsProvider() {
 
         result.newRow().apply {
             add(DocumentsContract.Root.COLUMN_ROOT_ID, "backup")
-            add(DocumentsContract.Root.COLUMN_TITLE, "备份盘")
-            add(DocumentsContract.Root.COLUMN_SUMMARY, "阿里云盘")
+            add(DocumentsContract.Root.COLUMN_TITLE, "阿里云盘(备份盘)")
             add(DocumentsContract.Root.COLUMN_DOCUMENT_ID, backupDriveId)
-            add(DocumentsContract.Root.COLUMN_FLAGS, DocumentsContract.Root.FLAG_SUPPORTS_CREATE)
+            add(
+                DocumentsContract.Root.COLUMN_FLAGS,
+                DocumentsContract.Root.FLAG_SUPPORTS_CREATE or DocumentsContract.Root.FLAG_SUPPORTS_SEARCH or DocumentsContract.Root.FLAG_SUPPORTS_IS_CHILD
+            )
+            add(DocumentsContract.Root.COLUMN_MIME_TYPES, "*/*")
         }
         result.newRow().apply {
             add(DocumentsContract.Root.COLUMN_ROOT_ID, "resource")
-            add(DocumentsContract.Root.COLUMN_TITLE, "资源盘")
-            add(DocumentsContract.Root.COLUMN_SUMMARY, "阿里云盘")
+            add(DocumentsContract.Root.COLUMN_TITLE, "阿里云盘(资源盘)")
             add(DocumentsContract.Root.COLUMN_DOCUMENT_ID, resourceDriveId)
-            add(DocumentsContract.Root.COLUMN_FLAGS, DocumentsContract.Root.FLAG_SUPPORTS_CREATE)
+            add(
+                DocumentsContract.Root.COLUMN_FLAGS,
+                DocumentsContract.Root.FLAG_SUPPORTS_CREATE or DocumentsContract.Root.FLAG_SUPPORTS_SEARCH or DocumentsContract.Root.FLAG_SUPPORTS_IS_CHILD
+            )
+            add(DocumentsContract.Root.COLUMN_MIME_TYPES, "*/*")
         }
 
         return result
@@ -98,25 +128,14 @@ class AliyunpanProvider : DocumentsProvider() {
                 )
                 add(
                     DocumentsContract.Document.COLUMN_DISPLAY_NAME,
-                    if (documentId == backupDriveId) "备份盘" else "资源盘"
+                    if (documentId == backupDriveId) "阿里云盘(备份盘)" else "阿里云盘(资源盘)"
                 )
             }
         } else if (documentId != "null") {
             try {
                 val (driveId, fileId) = getDriveIdAndFileId(documentId)
-                val fileItem = client.getFile(authorization, driveId, fileId).get()
-
-                result.newRow().apply {
-                    add(
-                        DocumentsContract.Document.COLUMN_DOCUMENT_ID,
-                        "$driveId-${fileItem.fileId}"
-                    )
-                    add(
-                        DocumentsContract.Document.COLUMN_MIME_TYPE,
-                        fileItem.mimeType ?: DocumentsContract.Document.MIME_TYPE_DIR
-                    )
-                    add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, fileItem.name)
-                }
+                val fileItem = client.getFile(authorization!!, driveId, fileId).get()
+                includeFile(result, fileItem)
             } catch (e: Exception) {
                 Log.e("AliyunpanProvider", "Failed to get file", e)
             }
@@ -141,17 +160,7 @@ class AliyunpanProvider : DocumentsProvider() {
                         .get()
 
                 listFileResponse.items.forEach { item ->
-                    result.newRow().apply {
-                        add(
-                            DocumentsContract.Document.COLUMN_DOCUMENT_ID,
-                            "$driveId-${item.fileId}"
-                        )
-                        add(
-                            DocumentsContract.Document.COLUMN_MIME_TYPE,
-                            item.mimeType ?: DocumentsContract.Document.MIME_TYPE_DIR
-                        )
-                        add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, item.name)
-                    }
+                    includeFile(result, item)
                 }
 
                 nextMarker = listFileResponse.nextMarker
@@ -183,9 +192,34 @@ class AliyunpanProvider : DocumentsProvider() {
             downloadFile(getDownloadUrlResponse.url, tempFile)
             return ParcelFileDescriptor.open(tempFile, ParcelFileDescriptor.MODE_READ_ONLY)
         } catch (e: Exception) {
-            Log.e("AliyunpanProvider", "Failed to open document", e)
+            Log.e("AliyunpanProvider", "Failed to open document $documentId", e)
             throw e
         }
+    }
+
+    override fun openDocumentThumbnail(
+        documentId: String?,
+        sizeHint: Point?,
+        signal: CancellationSignal?
+    ): AssetFileDescriptor? {
+        Log.d("AliyunpanProvider", "openDocumentThumbnail: $documentId")
+
+        val cacheDir = context?.cacheDir
+        val tempFile = File.createTempFile(documentId!!, ".thumbnail", cacheDir)
+
+        if (tempFile.length() == 0L) {
+            try {
+                val (driveId, fileId) = getDriveIdAndFileId(documentId)
+                val fileItem = client.getFile(authorization, driveId, fileId).get()
+
+                downloadFile(fileItem.thumbnail!!, tempFile)
+            } catch (e: Exception) {
+                Log.e("AliyunpanProvider", "Failed to get thumbnail $documentId", e)
+                throw e
+            }
+        }
+        val pfd = ParcelFileDescriptor.open(tempFile, ParcelFileDescriptor.MODE_READ_ONLY)
+        return AssetFileDescriptor(pfd, 0, tempFile.length())
     }
 
     private fun getDriveIdAndFileId(documentId: String?): Pair<String?, String?> {
@@ -201,8 +235,38 @@ class AliyunpanProvider : DocumentsProvider() {
         }
     }
 
+    private fun includeFile(result: MatrixCursor, fileItem: FileItem) {
+        var flags = 0
+        if (fileItem.type == "folder") {
+            flags = flags or DocumentsContract.Document.FLAG_DIR_SUPPORTS_CREATE
+        }
+        if (fileItem.thumbnail?.isNotEmpty() == true) {
+            flags = flags or DocumentsContract.Document.FLAG_SUPPORTS_THUMBNAIL
+        }
+
+        result.newRow().apply {
+            add(
+                DocumentsContract.Document.COLUMN_DOCUMENT_ID,
+                "${fileItem.driveId}-${fileItem.fileId}"
+            )
+            add(
+                DocumentsContract.Document.COLUMN_MIME_TYPE,
+                fileItem.mimeType ?: DocumentsContract.Document.MIME_TYPE_DIR
+            )
+            add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, fileItem.name)
+            add(DocumentsContract.Document.COLUMN_FLAGS, flags)
+            add(DocumentsContract.Document.COLUMN_SIZE, fileItem.size)
+            add(DocumentsContract.Document.COLUMN_LAST_MODIFIED, fileItem.updatedAt.toTimestamp())
+        }
+    }
+
     private fun downloadFile(downloadUrl: String, tempFile: File) {
-        val response = OkHttpClient().newCall(Request.Builder().url(downloadUrl).build()).execute()
+        val response = OkHttpClient().newCall(
+            Request.Builder().url(downloadUrl)
+                .addHeader("Referer", "https://www.alipan.com/")
+                .build()
+        ).execute()
+
         if (response.isSuccessful) {
             response.body?.byteStream()?.use { inputStream ->
                 tempFile.outputStream().use { outputStream ->
@@ -213,5 +277,9 @@ class AliyunpanProvider : DocumentsProvider() {
             Log.e("AliyunpanProvider", "Failed to download file: ${response.body}")
             throw FileNotFoundException("${response.body}")
         }
+    }
+
+    private fun String.toTimestamp(): Long {
+        return Instant.parse(this).toEpochMilli()
     }
 }
